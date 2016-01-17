f894a470c01cff2cb602e0da898b021ec051cbeb
Mohammad Moslem Uddin <pal.uddin@gmail.com>
2016-01-17 13:12:23 +0600

    Initial Commit

---
+++ Assets/Scripts/MoveLeftandRight.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveLeftandRight : MonoBehaviour {
+	public float speed = 1.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		transform.position += Vector3.right*Input.GetAxis("Horizontal")*speed*Time.deltaTime;
+	}
+}
