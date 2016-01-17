467cbc5138f8e4d5f85a155789a1f11cbc5e9b7b
Mohammad Moslem Uddin <pal.uddin@gmail.com>
2016-01-17 15:31:29 +0600

    Explode

---
+++ Assets/Scripts/GibOnTriggers.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GibOnTriggers : MonoBehaviour {
+
+
+	public GameObject gib;
+
+	void OnTriggerEnter()
+	{
+		Instantiate (gib, transform.position, Quaternion.identity);
+		Destroy (gameObject);
+
+	}
+
+
+
+}
